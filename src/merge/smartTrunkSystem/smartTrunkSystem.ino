3fa028774356c49a48216d1396e3601cc37f642f
lopeace <73591643+lopeace@users.noreply.github.com>
2022-04-18 13:34:07 +0900

    통합 테스트 코드
    전체 기능 수행 체크 완료.
    시리얼 모니터 제거 후 모터 동작 확인 요망.

--- src/merge/smartTrunkSystem/smartTrunkSystem.ino
+++ src/merge/smartTrunkSystem/smartTrunkSystem.ino
@@ -1,16 +1,5 @@
-/******** 자동 생성된 코드 *********/
-
-/* LMU uninitialised data */
-StartOfUninitialised_LMURam_Variables
-/* Put your LMU RAM fast access variables that have no initial values here e.g. uint32 LMU_var; */
-EndOfUninitialised_LMURam_Variables
-
-/* LMU uninitialised data */
-StartOfInitialised_LMURam_Variables
-/* Put your LMU RAM fast access variables that have an initial value here e.g. uint32 LMU_var_init = 1; */
-EndOfInitialised_LMURam_Variables
-
-/********** 작성한 코드 *********/
+#include <Stepper.h>
+#include <ezBuzzer.h> // ezBuzzer library
 
 // State condition flag definition 
 #define STATE_IDLE 0
@@ -22,44 +11,125 @@ EndOfInitialised_LMURam_Variables
 #define ON 1
 #define CLOSE 0
 #define OPEN 1
+#define NIGHT 40
+#define ANGLE_MAX 50
+#define ANGLE_MIN 0
 
 // Pin mapping
+#define RED 22
+#define BLUE 24
+#define GREEN 26
+#define LIGHT A1
+#define TRUNK_KEY 48
+#define SMART_KEY 49
+#define TRIGPIN 50
+#define ECHOPIN 51
 
+#define pwmA 3
+#define pwmB 11
+#define brakeA 9
+#define brakeB 8
+#define dirA 12
+#define dirB 13
+#define PUSH_SW  2
 
+#define BUZZER_PIN 5
+
+/*Ultrasonic Sensor (HC-SR04)*/
+//ultrasonic speed : 340m/s
+float duration, distance;
 
 // Global variables declaration
 int flag_state = STATE_IDLE;            // flag_State = 구동시작, 구동중, 구동완료 구분 플래그    STATE_ILDE(0) = 구동 완료/대기  STATE_BEGIN(1) = 구동 시작    STATE_RUN(2) = 구동 중
 int flag_open = CLOSE;                  // flag_Open = 열림 상태 구분 플래그                     CLOSE(0) = 닫힘           OPEN(1) = 열림
+int stepper_cnt = 0;
+
+
+// Define number of steps per revolution:
+const int stepsPerRevolution = 200;
+
+int melody1[] = {
+  NOTE_G5
+};
+int melody2[] = {
+  NOTE_E5, NOTE_E5, NOTE_E5
+};
+int noteDurations1[] = {
+  2
+};
+int noteDurations2[] = {
+  8, 8, 8
+};
+int noteLength1,noteLength2;
+
 
 // Functions declaration
 void event_key_input(void);
 void led_off(int pin_num);
 void led_on(int pin_num);
 
+bool trunk_key(void);
+bool smart_key(void);
 bool trunk_key_judge(void);
 bool ultrasonic_key_judge(void);
 bool smart_key_judge(void);
 bool get_light_sensor(void);
 
-int get_ultrasonic_dist(void);
+bool get_ultrasonic_dist(void);
 void set_stepper(int condition);
 void buzzer_beep(int condition);
+void state_led(int state);
+
+Stepper myStepper = Stepper(stepsPerRevolution, dirA, dirB);
+ezBuzzer buzzer(BUZZER_PIN);
 
 void setup() {
   // put your setup code for core 0 here, to run once:
+  pinMode(RED,OUTPUT);
+  pinMode(BLUE,OUTPUT);
+  pinMode(GREEN,OUTPUT);
+  pinMode(LIGHT,INPUT);
+  pinMode(TRUNK_KEY,INPUT);
+  pinMode(SMART_KEY,INPUT);
+  pinMode(TRIGPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  SerialASC.begin(9600);
+  analogReadResolution(12);
 
+  // Stepper motor pin mode setup
+  pinMode(pwmA, OUTPUT);
+  pinMode(pwmB, OUTPUT);
+  pinMode(brakeA, OUTPUT);
+  pinMode(brakeB, OUTPUT);
+  digitalWrite(pwmA, HIGH);
+  digitalWrite(pwmB, HIGH);
+  digitalWrite(brakeA, LOW);
+  digitalWrite(brakeB, LOW);
+  pinMode(PUSH_SW, INPUT);    // Direction pin on channel A
+  // Set the motor speed (RPMs):
+  myStepper.setSpeed(30);
+
+  noteLength1 = sizeof(noteDurations1) / sizeof(int);
+  noteLength2 = sizeof(noteDurations2) / sizeof(int);
+  
 }
 
 
 void loop() {
+  buzzer.loop();
+  
   // Key input judgement
-  if (trunk_key == HIGH){                // trunck key 눌림
-    if (trunk_key_judge() == true)   event_key_input();          // 10ms간 눌림 확인. 조건 미충족시 바로 return FALSE 로 나올 것
+  if (trunk_key() == true){                // trunck key 눌림
+    SerialASC.println("trunk key");
+    if (trunk_key_judge() == true)   event_key_input();          // 100ms간 눌림 확인. 조건 미충족시 바로 return FALSE 로 나올 것
+    delay(100);
   }
-  else if (get_ultrasonic_dist() < 20){   // 초음파 거리 20 미만 확인
+  else if (get_ultrasonic_dist() == true){   // 초음파 거리 20 미만 확인
+    SerialASC.println("ultrasonic");
     if (ultrasonic_key_judge() == true)   event_key_input();      // 초음파 거리 20 미만 0.3s 유지 확인. 조건 미충족시 바로 return FALSE 로 나올 것
   }
-  else if (smart_key == HIGH){            // smart key 눌림
+  else if (smart_key() == true){            // smart key 눌림
+    SerialASC.println("smart key");
     if (smart_key_judge() == true)    event_key_input();          // 3s간 눌림 확인. 조건 미충족시 바로 return FALSE 로 나올 것
   }
   else {                                  // 입력 장치 모두 미작동
@@ -68,44 +138,44 @@ void loop() {
   
   // State condition
   if (flag_state == STATE_RUN){                       // STATE_RUN      구동 중인 상태
-    
+    SerialASC.println("State run");
     if (flag_open == OPEN){                           // 열리는 상태
       set_stepper(OPEN);                              // 열리는 포지션으로 가는 코드
-      if (finish_step() == true)  flag_state = STATE_IDLE;    // 스텝모터가 목표위치 도달하면 구동 완료 상태로 변경
+      stepper_cnt++;
+      if(stepper_cnt >= ANGLE_MAX) {flag_state = STATE_IDLE; flag_open = OPEN; stepper_cnt = ANGLE_MAX;}     // 스텝모터가 목표위치 도달하면 구동 완료 상태로 변경
     }
     else{                                             // 닫히는 상태
       set_stepper(CLOSE);                             // 열리는 포지션으로 가는 코드
-      if (finish_step() == true)  flag_state = STATE_IDLE;    // 스텝모터가 목표위치 도달하면 구동 완료 상태로 변경
+      stepper_cnt--;
+      if(stepper_cnt <= ANGLE_MIN) {flag_state = STATE_IDLE; flag_open = CLOSE; stepper_cnt = ANGLE_MIN;}    // 스텝모터가 목표위치 도달하면 구동 완료 상태로 변경
     }
      
   }
   else if (flag_state == STATE_BEGIN){                // STATE_BEGIN    구동 시작하는 상태
-    
+    SerialASC.println("State begin");
     if (flag_open == OPEN){                           // 열린 상태
+      SerialASC.println("open");
       set_stepper(OPEN);                              // 열리는 포지션으로 가는 코드
+      stepper_cnt++;
       buzzer_beep(OPEN);                              // 열릴 때 비프음 출력
+      
       flag_state = STATE_RUN;                         // 구동 상태를 구동 중으로 변경
     }
     else{                                             // 닫힌 상태
+      SerialASC.println("close");
       set_stepper(CLOSE);                             // 닫히는 포지션으로 가는 코드
+      stepper_cnt--;
       buzzer_beep(CLOSE);                             // 닫힐 때 비프음 출력
+      
       flag_state = STATE_RUN;                         // 구동 상태를 구동 중으로 변경
     }
   }
   else{                                               // STATE_IDLE     구동 완료 or 대기 상태
-    
-    if (flage_open == OPEN){                          // 열린 상태
-      led_off(RED);
-      led_on(BLUE);                                   // 열린 상태면 파란불
-      if (get_light_sensor() == true) led_on(GREEN);  // 조도값이 일정 아래면 초록불
-      else  led_off(GREEN);                           // 조도값이 일정 이상이면 초록불 off
-    }
-    else{                                             // 닫힌 상태
-      led_off(GREEN);                                 // 닫힌 상태면 초록불 off
-      led_off(BLUE);
-      led_on(RED);                                    // 닫힌 상태면 빨간불
-    }
+    SerialASC.println("State idle");
+    state_led(flag_open);
   }
+
+  delay(100);
 }
 
 void event_key_input(void){
@@ -126,41 +196,165 @@ void led_on(int pin_num){
   digitalWrite(pin_num, HIGH);
 }
 
-bool trunk_key_judge(void){
-  // trunk key 입력을 100ms 간 체크하는 함수. 확인되면 true 반환. 체크 중 입력이 LOW가 되면 바로 false 반환할 것.
-  // empty function. please add contents after unit-test.
-  return true;
-  
+
+bool trunk_key(void){
+  if (digitalRead(TRUNK_KEY) == LOW) 
+  {  
+    return true;
+  }
+  else
+  { 
+    return false;
+  }
 }
-bool ultrasonic_key_judge(void){
-  // 초음파 센서 거리값이 20cm 미만임을 1s 간 체크하는 함수. 확인되면 true 반환. 체크 중 입력이 20 이상이 되면 바로 false 반환할 것.
-  // stub function. please add contents after unit-test.
+bool smart_key(void){
+  if (digitalRead(SMART_KEY) == LOW) 
+  {  
+    return true;
+  }
+  else
+  { 
+    return false;
+  }
+}
+bool trunk_key_judge(void){
+  // trunk key 입력을 10ms 간 체크하는 함수. 확인되면 true 반환. 체크 중 입력이 LOW가 되면 바로 false 반환할 것.
+  for (int cnt = 0; cnt < 10; ++cnt)
+  {
+    if (trunk_key() == true) //스위치가 눌린 경우
+    {
+      delay(10);                   //출력을 위한 딜레이
+      continue;
+    }
+    else
+    { 
+      delay(10);                   //출력을 위한 딜레이
+      return false;
+    }
+  }
   return true;
 }
 bool smart_key_judge(void){
   // smart key 입력을 3s 간 체크하는 함수. 확인되면 true 반환. 체크 중 입력이 LOW가 되면 바로 false 반환할 것.
-  // stub function. please add contents after unit-test.
+    for (int cnt = 0; cnt < 300; ++cnt)
+  { 
+    if (smart_key() == true) 
+    {
+      delay(10);                 
+      continue;
+    }
+    else
+    { 
+      delay(10);                 
+      return false;
+    }
+  }
   return true;
 }
-bool get_light_sensor(void){
-  // 조도 센서값이 임계값 미만인지 확인하는 함수. 미만이면 true 반환. 이상이면 false 반환.
-  // stub function. please add contents after unit-test.
+bool ultrasonic_key_judge(void){
+  // 초음파 센서 거리값이 20cm 미만임을 1s 간 체크하는 함수. 확인되면 true 반환. 체크 중 입력이 20 이상이 되면 바로 false 반환할 것.
+  for (int cnt=0;cnt<30;++cnt)
+  {
+    digitalWrite(TRIGPIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIGPIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGPIN, LOW); 
+    // Echo 핀으로 들어온 펄스 시간 (us) 측정 
+    //pulseIn(pin, value, timeout)
+    //pin :  the number of the Arduino pin on which you want to read the pulse.
+    //value: type of pulse to read: either HIGH or LOW
+    //timeout (optional): the number of microseconds to wait for the pulse to start; 
+    // --> default is one second, unsigned long
+    duration = pulseIn(ECHOPIN, HIGH, 11000); //time[us]
+    distance = ((float)(duration)*0.34/10)/2; //time[us]*speed[cm/us]
+    if (distance < 20 && distance > 1)
+    {
+      delay(10);
+      continue;
+    }
+    else
+    { 
+      delay(10);
+      return false;
+    }
+  }
   return true;
+}
+bool get_light_sensor(int pinnum){
+  // 조도 센서값이 임계값 미만인지 확인하는 함수. 미만이면 true 반환. 이상이면 false 반환.
+  int light_data = analogRead(pinnum);
+  if (light_data < NIGHT)  return true;
+  else  return false;
 }
 
-int get_ultrasonic_dist(void){
-  // 초음파 센서로 인식한 거리(cm)를 integer 형으로 반환하는 함수.
-  // stub function. please add contents after unit-test.
-  return 10;
+
+bool get_ultrasonic_dist(void) {
+  // 0.01초 간격으로 거리가 20cm이내인 경우 true 아니면 false를 출력하는 함수
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW); 
+  // Echo 핀으로 들어온 펄스 시간 (us) 측정 
+  //pulseIn(pin, value, timeout)
+  //pin :  the number of the Arduino pin on which you want to read the pulse.
+  //value: type of pulse to read: either HIGH or LOW
+  //timeout (optional): the number of microseconds to wait for the pulse to start; 
+  // --> default is one second, unsigned long
+  duration = pulseIn(ECHOPIN, HIGH, 11000); //time[us]
+  distance = ((float)(duration)*0.34/10)/2; //time[us]*speed[cm/us]
+  if (distance < 20 && distance > 1)
+  {
+    delay(10);
+  }
+  else
+  { 
+    delay(10);
+    return false;
+  }
+  return true;
 }
 void set_stepper(int condition){
   // stepper motor의 구동을 condition(OPEN/CLOSE)에 따라 제어하는 함수. 정해진 위치까지 loop마다 일정하게 회전하도록 작동할 것.
-  // stub function. please add contents after unit-test.
   
+  if (condition == OPEN){
+    myStepper.step(-1);
+    delay(10);
+  }
+  if (condition == CLOSE){
+    myStepper.step(1);
+    delay(10);
+
+  }
 }
 void buzzer_beep(int condition){
   // buzzer가 입력받은 condition(OPEN/CLOSE)에 따라 정해진 음을 작동시키는 함수. OPEN시 3회 비프음. CLOSE시 1회 장음.
-  // stub function. please add contents after unit-test.
-  
+  if(condition == 1){
+      if (buzzer.getState() == BUZZER_IDLE) {
+      SerialASC.println("open");
+      buzzer.playMelody(melody1, noteDurations1, noteLength1); // playing
+      SerialASC.println("open stop");
+    }
+    }
+  else if(condition == 0){
+      if (buzzer.getState() == BUZZER_IDLE) {
+        SerialASC.println("close");
+      buzzer.playMelody(melody2, noteDurations2, noteLength2); // playing
+      SerialASC.println("close stop");
+    }
+    }
+  }
+void state_led(int state){
+  if (state == OPEN){
+    led_off(RED);
+    led_on(BLUE);                                   // 열린 상태면 파란불
+    if (get_light_sensor(LIGHT) == true) led_on(GREEN);  // 조도값이 일정 아래면 초록불
+    else  led_off(GREEN);                           // 조도값이 일정 이상이면 초록불 off
+  }
+  else{
+    led_off(GREEN);                                 // 닫힌 상태면 초록불 off
+    led_off(BLUE);
+    led_on(RED);                                    // 닫힌 상태면 빨간불
+  }
 }
-
