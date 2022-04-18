d8f108464f0e0aaf878846ec2194feebc46c599f
holysid <45979433+holysid@users.noreply.github.com>
2022-04-18 10:59:23 +0900

    스위치 작동 감지 함수
    D2(trunck_key_1.0.ino), D3(smart_key_1.0.ino) 스위치의 작동을 1회 감지하는 함수 추가

---
+++ src/baseline/smart_key_1.0.ino
@@ -0,0 +1,18 @@
+#define smart_key 3
+
+void setup() {
+  pinMode(smart_key, INPUT);  // D3 switch setup
+}
+
+//D3스위치가 눌리는 경우 true 아닐 경우 false를 출력하는 함수
+bool smart_key() {
+
+  if (digitalRead(smart_key) == LOW) 
+  {  
+    return true;
+  }
+  else
+  { 
+    return false;
+  }
+}
---
+++ src/baseline/trunck_key_1.0.ino
@@ -0,0 +1,18 @@
+#define trunck_key 2
+
+void setup() {
+  pinMode(trunck_key, INPUT); // D2 switch setup
+}
+
+// truck_key가 눌리는 경우 true를 출력
+bool truck_key() {
+
+  if (digitalRead(trunck_key) == LOW) //스위치가 눌린 경우
+  {  
+    return true;
+  }
+  else
+  { 
+    return false;
+  }
+}
