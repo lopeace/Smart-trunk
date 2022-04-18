af4fb2b5ce3d522b535ad84454441b621173b417
holysid <45979433+holysid@users.noreply.github.com>
2022-04-18 10:43:00 +0900

    주석 추가 및 감지 시간 변경
    모든 파일 주석 추가
    D2, D3 스위치의 작동 감지 시간을 3초로 변경
    거리 감지 시간을 1초로 변경

---
+++ src/unit_test/get_ultrasonic_dist_1.1.ino
@@ -0,0 +1,44 @@
+#define trigPin 50
+#define echoPin 51
+/*Ultrasonic Sensor (HC-SR04)*/
+//ultrasonic speed : 340m/s
+float duration, distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  SerialASC.begin(9600);
+}
+
+//거리가 20cm 이하인 경우가 1초 이상 지속되면 TRUE를 출력(return)하며 아닌 경우 0.1초 간격으로 FALSE를 출력(return)
+bool loop() {
+  for (int cnt=0;cnt<10;++cnt)
+  {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW); 
+    // Echo 핀으로 들어온 펄스 시간 (us) 측정 
+    //pulseIn(pin, value, timeout)
+    //pin :  the number of the Arduino pin on which you want to read the pulse.
+    //value: type of pulse to read: either HIGH or LOW
+    //timeout (optional): the number of microseconds to wait for the pulse to start; 
+    // --> default is one second, unsigned long
+    duration = pulseIn(echoPin, HIGH, 11000); //time[us]
+    distance = ((float)(duration)*0.34/10)/2; //time[us]*speed[cm/us] = cm
+    if (distance < 20)
+    {
+      delay(100);                 //0.1초의 딜레이
+      continue;
+    }
+    else
+    { 
+      delay(100);
+      SerialASC.println("FALSE"); //20cm 이상인 경우 FALSE를 출력
+      return false;               //false를 return
+    }
+  }
+  SerialASC.println("TRUE");      //20cm 이하로 1초 이상인 경우 TRUE를 출력
+  return true;                    //true를 return
+}
---
+++ src/unit_test/smart_key_judge_1.1.ino
@@ -0,0 +1,26 @@
+#define smart_key 3
+
+void setup() {
+  pinMode(smart_key, INPUT);  // D3 switch setup
+  SerialASC.begin(9600);      // 출력을 위한 설정
+}
+
+// 3초 이상 D3 스위치를 누르는 경우 TRUE 출력 아닌 경우 0.1초 간격으로 
+bool loop() {
+  for (int cnt = 0; cnt < 30; ++cnt)
+  { 
+    if (digitalRead(smart_key) == LOW) 
+    {
+      delay(100);                 //0.1초의 딜레이
+      continue;
+    }
+    else
+    { 
+      delay(100);                 //0.1초의 딜레이
+      SerialASC.println("FALSE"); //"FALSE" 출력 
+      return false;               //false를 return
+    }
+  }
+  SerialASC.println("TURE");  //"TRUE" 출력 
+  return true;                //true를 reture
+}
