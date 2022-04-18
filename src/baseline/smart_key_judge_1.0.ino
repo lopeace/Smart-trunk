e09862b0c4fa3bd7f8b1c05a83d9f4ef8333bc3d
holysid <45979433+holysid@users.noreply.github.com>
2022-04-18 10:12:26 +0900

    Add files via upload
    베이스라인 1.0 담당 함수 추가

---
+++ src/baseline/smart_key_judge_1.0.ino
@@ -0,0 +1,23 @@
+#define smart_key 3
+
+void setup() {
+  pinMode(smart_key, INPUT);  // D3 switch setup
+}
+
+// 스위치가 3초 이상 눌리는 경우 true 아닐 경우 false를 출력하는 함수
+bool smart_key_judge() {
+  for (int cnt = 0; cnt < 300; ++cnt)
+  { 
+    if (digitalRead(smart_key) == LOW) 
+    {
+      delay(10);                 
+      continue;
+    }
+    else
+    { 
+      delay(10);                 
+      return false;
+    }
+  }
+  return true;
+}
---
+++ src/baseline/trunck_key_judge_1.0.ino
@@ -0,0 +1,23 @@
+#define trunck_key 2
+
+void setup() {
+  pinMode(trunck_key, INPUT); // D2 switch setup
+}
+
+// truck_key가 0.01초 이상 눌리는 경우
+bool truck_key_judge() {
+  for (int cnt = 0; cnt > 10; ++cnt)
+  {
+    if (digitalRead(trunck_key) == LOW) //스위치가 눌린 경우
+    {
+      delay(1);                   //출력을 위한 딜레이
+      continue;
+    }
+    else
+    { 
+      delay(1);                   //출력을 위한 딜레이
+      return false;
+    }
+  }
+  return true;
+}
