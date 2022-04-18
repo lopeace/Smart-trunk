7dc865cca7c968f38238686749f84f9a0fb4e3bb
holysid <45979433+holysid@users.noreply.github.com>
2022-04-18 10:55:23 +0900

    함수의 기능 오류 수정
    거리를 1회만 감지하여 return하는 함수로 변경

--- src/baseline/get_ultrasonic_dist_1.0.ino
+++ src/baseline/get_ultrasonic_dist_1.0.ino
@@ -10,33 +10,30 @@ void setup() {
   SerialASC.begin(9600);
 }
 
-// 0.3초 지속되면 true 아니면 false를 출력하는 함수
+// 0.01초 간격으로 거리가 20cm이내인 경우 true 아니면 false를 출력하는 함수
 bool get_ultrasonic_dist() {
-  for (int cnt=0;cnt<30;++cnt)
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW); 
+  // Echo 핀으로 들어온 펄스 시간 (us) 측정 
+  //pulseIn(pin, value, timeout)
+  //pin :  the number of the Arduino pin on which you want to read the pulse.
+  //value: type of pulse to read: either HIGH or LOW
+  //timeout (optional): the number of microseconds to wait for the pulse to start; 
+  // --> default is one second, unsigned long
+  duration = pulseIn(echoPin, HIGH, 11000); //time[us]
+  distance = ((float)(duration)*0.34/10)/2; //time[us]*speed[cm/us]
+  if (distance < 20)
   {
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW); 
-    // Echo 핀으로 들어온 펄스 시간 (us) 측정 
-    //pulseIn(pin, value, timeout)
-    //pin :  the number of the Arduino pin on which you want to read the pulse.
-    //value: type of pulse to read: either HIGH or LOW
-    //timeout (optional): the number of microseconds to wait for the pulse to start; 
-    // --> default is one second, unsigned long
-    duration = pulseIn(echoPin, HIGH, 11000); //time[us]
-    distance = ((float)(duration)*0.34/10)/2; //time[us]*speed[cm/us]
-    if (distance < 20)
-    {
-      delay(10);
-      continue;
-    }
-    else
-    { 
-      delay(10);
-      return false;
-    }
+    delay(10);
+    continue;
+  }
+  else
+  { 
+    delay(10);
+    return false;
   }
   return true;
 }
